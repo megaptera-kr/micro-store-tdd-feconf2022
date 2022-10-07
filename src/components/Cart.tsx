6371141d8f0de2a09b57192c88a4246247dae1fd
wholemann <wholeman.dev@gmail.com>
2022-10-08 04:23:52 +0900

    Starting point of Example

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,27 +1,31 @@
-import useCartStore from '../hooks/useCartStore';
+// // import useCartStore from '../hooks/useCartStore';
 
-export default function Cart() {
-  // TODO: CartStore의 snapshot 얻기
-  const [snapshot] = useCartStore();
-  const { items } = snapshot;
+// export default function Cart() {
+//   // TODO: CartStore의 snapshot 얻기
+//   // const [snapshot] = useCartStore();
+//   // const { items } = snapshot;
 
-  return (
-    <div>
-      <h2>카트</h2>
-      {items.length ? (
-        <ul>
-          {items.map((item) => (
-            <li key={item.id}>
-              상품 #
-              {item.productId}
-              {' - '}
-              {item.quantity}
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <p>장바구니가 비어있습니다</p>
-      )}
-    </div>
-  );
-}
+//   const items = [
+//     { id: 1, productId: 1, quantity: 1 },
+//   ];
+
+//   return (
+//     <div>
+//       <h2>카트</h2>
+//       {items.length ? (
+//         <ul>
+//           {items.map((item) => (
+//             <li key={item.id}>
+//               상품 #
+//               {item.productId}
+//               {' - '}
+//               {item.quantity}
+//             </li>
+//           ))}
+//         </ul>
+//       ) : (
+//         <p>장바구니가 비어있습니다</p>
+//       )}
+//     </div>
+//   );
+// }
